2a53d1faaaebec39dcc64fec9b482904ffcd40a5
praddv <62784799+praddv@users.noreply.github.com>
2020-05-08 19:49:31 +0530

    Add files via upload

---
+++ Digging_deeper_in_python_assignment.py
@@ -0,0 +1,23 @@
+#assigning elements to lists
+laptops = ['HP', 'Dell', 'Lenovo']
+print(laptops)
+
+laptops.append ('Apple')
+print (laptops)
+
+#accessing elements from a tuple
+t1 = ("a", "b")
+print(t1[0])
+
+#deleting different directory elements
+
+d1 = {
+    'MacOS':'Cool',
+    'Linux':'Functional',
+    'Windows':'Crap'
+}
+print (d1)
+
+
+del d1["Windows"]
+print(d1)
